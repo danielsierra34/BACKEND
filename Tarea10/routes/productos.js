1c74606483e52ab80e73ded9b30ab09d2a312fb6
danielsierra34 <danielsierra34@gmail.com>
2021-06-14 01:04:54 -0500

    tarea 10

---
+++ Tarea10/routes/productos.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const router = express.Router()
+
+const Productos = require('../api/productos')
+const instancia = new Productos("productos.txt");
+
+//router.use(express.static('public'));
+
+router.get('/',(request,response)=>{
+    response.render('bienvenida')
+})
+
+router.get('/vista',(request,response) => {
+    try{
+        response.render('main', {productos:instancia.read(),hayProductos:true})
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }
+})
+
+router.get('/crear',(request,response) => {
+    try{
+        response.render('createProduct')
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }
+})
+
+router.get('/listar',(request,response)=>{
+    try{
+        response.json({'items':instancia.read()})
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }    
+})
+
+router.get('/listar/:id',(request,response)=>{
+    console.log(request.params.id)
+    try{
+        response.json(instancia.read(request.params.id))
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }    
+})
+
+router.post('/guardar',(request,response)=>{
+    console.log(request)
+    try{
+        let producto=request.body
+        return response.json({"inserción":{"producto": producto}, "resultado": {"productos": instancia.write(producto)}})
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    } 
+    
+})
+
+router.put('/editar/:id',(request,response)=>{
+    try{
+        let producto=request.body
+        return response.json({"edición":{"producto": producto}, "resultado": {"productos": instancia.edit(producto,request.params.id)}})
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }   
+    
+})
+
+router.delete('/eliminar/:id',(request,response)=>{
+    console.log(request.params.id) 
+    try{
+        return response.json({"resultado": {"productos": instancia.delete(request.params.id)}})
+    }catch (error){
+        response.json({"error": "Intente de nuevo más tarde"})
+    }   
+    
+})
+
+module.exports = router
